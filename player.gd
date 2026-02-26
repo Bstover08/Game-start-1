64c2eecd98cfd1becdc813e53927b743f0214db3
Bstover08 <dthru58@gmail.com>
2026-02-26 14:06:52 -0500

    g

--- player.gd
+++ player.gd
@@ -2,10 +2,12 @@ extends CharacterBody2D
 
 
 const SPEED = 130
-const JUMP_VELOCITY = -300
+const JUMP_VELOCITY = -360
 
 
-func _physics_process(delta: float) -> void:
+
+func _physics_process(delta):
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
